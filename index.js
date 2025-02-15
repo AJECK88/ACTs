54d7f767024b0e0623e8575928951a29e3c4f239
AJECK88 <eajeckekizee@gmail.com>
2025-02-15 23:39:27 +0100

    versiom 1

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const login=document.querySelector('#login-content')
+const sigin = document.querySelector('#sigins')
+const callsingin= document.querySelector('#sigin-content')
+
+sigin.addEventListener('click', function(){
+    login.classList.add("active")
+    callsingin.classList.add("actives")
+    
+})
+
