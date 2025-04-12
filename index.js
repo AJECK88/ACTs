42b2b8d0f29382b1a3ec975efb6506c68f91078b
ajeck88 <eajeckelizee@gmail.com>
2025-04-12 12:38:29 +0100

    Adding and changing some features

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const display = document.querySelector(".EmergencyGrouping")
 
 display.addEventListener("click", ()=>{
     document.querySelector(".imergency").classList.toggle("active")
+      
 });
 
  function displays(){
@@ -11,6 +12,6 @@ display.addEventListener("click", ()=>{
  bar.innerHTML = bar.innerHTML != '<i class=" fa-solid fa-x"></i>' 
  ? '<i class=" fa-solid fa-x"></i>' : '<i class=" fa-solid fa-bars"></i>'
  document.querySelector(".left_nav").classList.toggle("active")
- console.log(document.querySelector(".left_nav"))
-    
  }
+ 
+ 
