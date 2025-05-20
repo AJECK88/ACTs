615e8033012550c44759ddc84c94cf093e23eecc
ajeck88 <eajeckelizee@gmail.com>
2025-05-20 12:05:34 +0100

    the emergency page and the lading page

---
+++ script/Emergency.js
@@ -0,0 +1,506 @@
+const africanEmergencyNumbers = {
+    Algeria: {
+        countryName: "Algeria",
+        flag: "https://flagcdn.com/w320/dz.png",
+        police: "17",
+        ambulance: "14",
+        fire: "14",
+        foreignEmergencyName: "Foreign Affairs Emergency",
+        foreignEmergencyNumber: "+213-21-92-02-20"
+    },
+    Angola: {
+        countryName: "Angola",
+        flag: "https://flagcdn.com/w320/ao.png",
+        police: "113",
+        ambulance: "112",
+        fire: "115",
+        foreignEmergencyName: "Foreign Nationals Assistance",
+        foreignEmergencyNumber: "+244-222-33-44-55"
+    },
+    Benin: {
+        countryName: "Benin",
+        flag: "https://flagcdn.com/w320/bj.png",
+        police: "117",
+        ambulance: "112",
+        fire: "118",
+        foreignEmergencyName: "Embassy Emergency Contact",
+        foreignEmergencyNumber: "+229-21-30-02-25"
+    },
+    Botswana: {
+        countryName: "Botswana",
+        flag: "https://flagcdn.com/w320/bw.png",
+        police: "999",
+        ambulance: "997",
+        fire: "998",
+        foreignEmergencyName: "Foreign Help Desk",
+        foreignEmergencyNumber: "+267-391-1444"
+    },
+    BurkinaFaso: {
+        countryName: "Burkina Faso",
+        flag: "https://flagcdn.com/w320/bf.png",
+        police: "17",
+        ambulance: "112",
+        fire: "18",
+        foreignEmergencyName: "International Assistance",
+        foreignEmergencyNumber: "+226-25-30-60-69"
+    },
+    Burundi: {
+        countryName: "Burundi",
+        flag: "https://flagcdn.com/w320/bi.png",
+        police: "112",
+        ambulance: "112",
+        fire: "112",
+        foreignEmergencyName: "Foreign Nationals Emergency",
+        foreignEmergencyNumber: "+257-22-22-27-99"
+    },
+    Cameroon: {
+        countryName: "Cameroon",
+        flag: "https://flagcdn.com/w320/cm.png",
+        police: "117",
+        ambulance: "112",
+        fire: "118",
+        foreignEmergencyName: "Embassy Support Line",
+        foreignEmergencyNumber: "+237-222-20-15-00"
+    },
+    CapeVerde: {
+        countryName: "Cape Verde",
+        flag: "https://flagcdn.com/w320/cv.png",
+        police: "132",
+        ambulance: "130",
+        fire: "131",
+        foreignEmergencyName: "Foreign Assistance Hotline",
+        foreignEmergencyNumber: "+238-260-14-00"
+    },
+    Chad: {
+        countryName: "Chad",
+        flag: "https://flagcdn.com/w320/td.png",
+        police: "17",
+        ambulance: "2251-4242",
+        fire: "18",
+        foreignEmergencyName: "International Emergency Contact",
+        foreignEmergencyNumber: "+235-22-51-50-50"
+    },
+    Comoros: {
+        countryName: "Comoros",
+        flag: "https://flagcdn.com/w320/km.png",
+        police: "17",
+        ambulance: "772-03-73",
+        fire: "18",
+        foreignEmergencyName: "Foreign Nationals Help",
+        foreignEmergencyNumber: "+269-773-09-09"
+    },
+    Congo: {
+        countryName: "Congo",
+        flag: "https://flagcdn.com/w320/cg.png",
+        police: "117",
+        ambulance: "118",
+        fire: "118",
+        foreignEmergencyName: "Embassy Emergency Line",
+        foreignEmergencyNumber: "+242-06-612-04-04"
+    },
+    Djibouti: {
+        countryName: "Djibouti",
+        flag: "https://flagcdn.com/w320/dj.png",
+        police: "17",
+        ambulance: "19",
+        fire: "18",
+        foreignEmergencyName: "Foreign Affairs Hotline",
+        foreignEmergencyNumber: "+253-21-35-09-99"
+    },
+    Egypt: {
+        countryName: "Egypt",
+        flag: "https://flagcdn.com/w320/eg.png",
+        police: "122",
+        ambulance: "123",
+        fire: "180",
+        foreignEmergencyName: "Tourist Emergency Assistance",
+        foreignEmergencyNumber: "+20-2-2794-9700"
+    },
+    Eswatini: {
+        countryName: "Eswatini",
+        flag: "https://flagcdn.com/w320/sz.png",
+        police: "999",
+        ambulance: "977",
+        fire: "933",
+        foreignEmergencyName: "Foreign Nationals Support",
+        foreignEmergencyNumber: "+268-2404-2000"
+    },
+    Ethiopia: {
+        countryName: "Ethiopia",
+        flag: "https://flagcdn.com/w320/et.png",
+        police: "911",
+        ambulance: "907",
+        fire: "939",
+        foreignEmergencyName: "Embassy Emergency Desk",
+        foreignEmergencyNumber: "+251-11-123-4567"
+    },
+    Gabon: {
+        countryName: "Gabon",
+        flag: "https://flagcdn.com/w320/ga.png",
+        police: "1730",
+        ambulance: "1300",
+        fire: "18",
+        foreignEmergencyName: "Foreign Emergency Hotline",
+        foreignEmergencyNumber: "+241-01-76-20-03"
+    },
+    Gambia: {
+        countryName: "Gambia",
+        flag: "https://flagcdn.com/w320/gm.png",
+        police: "117",
+        ambulance: "116",
+        fire: "118",
+        foreignEmergencyName: "International Assistance Line",
+        foreignEmergencyNumber: "+220-422-8232"
+    },
+    Ghana: {
+        countryName: "Ghana",
+        flag: "https://flagcdn.com/w320/gh.png",
+        police: "191",
+        ambulance: "193",
+        fire: "192",
+        foreignEmergencyName: "Foreign Nationals Emergency",
+        foreignEmergencyNumber: "+233-302-21-11-00"
+    },
+    Guinea: {
+        countryName: "Guinea",
+        flag: "https://flagcdn.com/w320/gn.png",
+        police: "122",
+        ambulance: "442-020",
+        fire: "18",
+        foreignEmergencyName: "Embassy Assistance Contact",
+        foreignEmergencyNumber: "+224-622-35-00-00"
+    },
+    Kenya: {
+        countryName: "Kenya",
+        flag: "https://flagcdn.com/w320/ke.png",
+        police: "999",
+        ambulance: "112",
+        fire: "911",
+        foreignEmergencyName: "Tourist Emergency Hotline",
+        foreignEmergencyNumber: "+254-20-363-6000"
+    },
+    Lesotho: {
+        countryName: "Lesotho",
+        flag: "https://flagcdn.com/w320/ls.png",
+        police: "123",
+        ambulance: "121",
+        fire: "122",
+        foreignEmergencyName: "Foreign Nationals Help Desk",
+        foreignEmergencyNumber: "+266-2231-2231"
+    },
+    Liberia: {
+        countryName: "Liberia",
+        flag: "https://flagcdn.com/w320/lr.png",
+        police: "911",
+        ambulance: "911",
+        fire: "911",
+        foreignEmergencyName: "Embassy Emergency Support",
+        foreignEmergencyNumber: "+231-77-677-7000"
+    },
+    Libya: {
+        countryName: "Libya",
+        flag: "https://flagcdn.com/w320/ly.png",
+        police: "1515",
+        ambulance: "1515",
+        fire: "1515",
+        foreignEmergencyName: "Foreign Affairs Emergency",
+        foreignEmergencyNumber: "+218-21-340-3644"
+    },
+    Madagascar: {
+        countryName: "Madagascar",
+        flag: "https://flagcdn.com/w320/mg.png",
+        police: "117",
+        ambulance: "124",
+        fire: "118",
+        foreignEmergencyName: "Tourist Assistance Hotline",
+        foreignEmergencyNumber: "+261-20-22-202-02"
+    },
+    Malawi: {
+        countryName: "Malawi",
+        flag: "https://flagcdn.com/w320/mw.png",
+        police: "997",
+        ambulance: "998",
+        fire: "999",
+        foreignEmergencyName: "Foreign Nationals Emergency",
+        foreignEmergencyNumber: "+265-1-772-222"
+    },
+    Mali: {
+        countryName: "Mali",
+        flag: "https://flagcdn.com/w320/ml.png",
+        police: "17",
+        ambulance: "15",
+        fire: "18",
+        foreignEmergencyName: "Embassy Emergency Contact",
+        foreignEmergencyNumber: "+223-20-22-22-22"
+    },
+    Mauritania: {
+        countryName: "Mauritania",
+        flag: "https://flagcdn.com/w320/mr.png",
+        police: "117",
+        ambulance: "118",
+        fire: "118",
+        foreignEmergencyName: "Foreign Affairs Hotline",
+        foreignEmergencyNumber: "+222-45-25-37-37"
+    },
+    Mauritius: {
+        countryName: "Mauritius",
+        flag: "https://flagcdn.com/w320/mu.png",
+        police: "999",
+        ambulance: "114",
+        fire: "115",
+        foreignEmergencyName: "Tourist Emergency Assistance",
+        foreignEmergencyNumber: "+230-202-0200"
+    },
+    Morocco: {
+        countryName: "Morocco",
+        flag: "https://flagcdn.com/w320/ma.png",
+        police: "19",
+        ambulance: "15",
+        fire: "15",
+        foreignEmergencyName: "Foreign Nationals Support",
+        foreignEmergencyNumber: "+212-537-63-33-33"
+    },
+    Mozambique: {
+        countryName: "Mozambique",
+        flag: "https://flagcdn.com/w320/mz.png",
+        police: "119",
+        ambulance: "117",
+        fire: "198",
+        foreignEmergencyName: "Embassy Emergency Line",
+        foreignEmergencyNumber: "+258-21-49-36-00"
+    },
+    Namibia: {
+        countryName: "Namibia",
+        flag: "https://flagcdn.com/w320/na.png",
+        police: "10111",
+        ambulance: "211111",
+        fire: "999",
+        foreignEmergencyName: "Foreign Affairs Emergency",
+        foreignEmergencyNumber: "+264-61-292-9111"
+    },
+    Niger: {
+        countryName: "Niger",
+        flag: "https://flagcdn.com/w320/ne.png",
+        police: "17",
+        ambulance: "15",
+        fire: "18",
+        foreignEmergencyName: "International Assistance Hotline",
+        foreignEmergencyNumber: "+227-20-72-25-53"
+    },
+    Nigeria: {
+        countryName: "Nigeria",
+        flag: "https://flagcdn.com/w320/ng.png",
+        police: "112",
+        ambulance: "112",
+        fire: "112",
+        foreignEmergencyName: "Foreign Nationals Emergency",
+        foreignEmergencyNumber: "+234-9-461-4000"
+    },
+    Rwanda: {
+        countryName: "Rwanda",
+        flag: "https://flagcdn.com/w320/rw.png",
+        police: "112",
+        ambulance: "912",
+        fire: "111",
+        foreignEmergencyName: "Embassy Support Hotline",
+        foreignEmergencyNumber: "+250-788-311-155"
+    },
+    Senegal: {
+        countryName: "Senegal",
+        flag: "https://flagcdn.com/w320/sn.png",
+        police: "17",
+        ambulance: "15",
+        fire: "18",
+        foreignEmergencyName: "Foreign Affairs Emergency",
+        foreignEmergencyNumber: "+221-33-889-8900"
+    },
+    Seychelles: {
+        countryName: "Seychelles",
+        flag: "https://flagcdn.com/w320/sc.png",
+        police: "999",
+        ambulance: "151",
+        fire: "999",
+        foreignEmergencyName: "Tourist Assistance Hotline",
+        foreignEmergencyNumber: "+248-4-28-00-00"
+    },
+    SierraLeone: {
+        countryName: "Sierra Leone",
+        flag: "https://flagcdn.com/w320/sl.png",
+        police: "999",
+        ambulance: "999",
+        fire: "999",
+        foreignEmergencyName: "Foreign Nationals Help Desk",
+        foreignEmergencyNumber: "+232-22-22-22-22"
+    },
+    Somalia: {
+        countryName: "Somalia",
+        flag: "https://flagcdn.com/w320/so.png",
+        police: "888",
+        ambulance: "999",
+        fire: "555",
+        foreignEmergencyName: "Embassy Emergency Contact",
+        foreignEmergencyNumber: "+252-61-555-5555"
+    },
+    SouthAfrica: {
+        countryName: "South Africa",
+        flag: "https://flagcdn.com/w320/za.png",
+        police: "10111",
+        ambulance: "10177",
+        fire: "10177",
+        foreignEmergencyName: "Tourist Emergency Assistance",
+        foreignEmergencyNumber: "+27-12-342-1000"
+    },
+    SouthSudan: {
+        countryName: "South Sudan",
+        flag: "https://flagcdn.com/w320/ss.png",
+        police: "777",
+        ambulance: "999",
+        fire: "999",
+        foreignEmergencyName: "Foreign Nationals Emergency",
+        foreignEmergencyNumber: "+211-912-123-456"
+    },
+    Sudan: {
+        countryName: "Sudan",
+        flag: "https://flagcdn.com/w320/sd.png",
+        police: "999",
+        ambulance: "333",
+        fire: "555",
+        foreignEmergencyName: "Embassy Emergency Hotline",
+        foreignEmergencyNumber: "+249-183-77-77-77"
+    },
+    Tanzania: {
+        countryName: "Tanzania",
+        flag: "https://flagcdn.com/w320/tz.png",
+        police: "112",
+        ambulance: "115",
+        fire: "114",
+        foreignEmergencyName: "Tourist Assistance Hotline",
+        foreignEmergencyNumber: "+255-22-211-1222"
+    },
+    Togo: {
+        countryName: "Togo",
+        flag: "https://flagcdn.com/w320/tg.png",
+        police: "117",
+        ambulance: "8200",
+        fire: "118",
+        foreignEmergencyName: "Foreign Nationals Support",
+        foreignEmergencyNumber: "+228-22-21-00-03"
+    },
+    Tunisia: {
+        countryName: "Tunisia",
+        flag: "https://flagcdn.com/w320/tn.png",
+        police: "197",
+        ambulance: "190",
+        fire: "198",
+        foreignEmergencyName: "Embassy Emergency Desk",
+        foreignEmergencyNumber: "+216-71-107-000"
+    },
+    Uganda: {
+        countryName: "Uganda",
+        flag: "https://flagcdn.com/w320/ug.png",
+        police: "999",
+        ambulance: "112",
+        fire: "999",
+        foreignEmergencyName: "Foreign Affairs Emergency",
+        foreignEmergencyNumber: "+256-414-256-000"
+    },
+    Zambia: {
+        countryName: "Zambia",
+        flag: "https://flagcdn.com/w320/zm.png",
+        police: "991",
+        ambulance: "992",
+        fire: "993",
+        foreignEmergencyName: "Foreign Nationals Help Desk",
+        foreignEmergencyNumber: "+260-211-25-00-00"
+    },
+    Zimbabwe: {
+        countryName: "Zimbabwe",
+        flag: "https://flagcdn.com/w320/zw.png",
+        police: "995",
+        ambulance: "994",
+        fire: "993",
+        foreignEmergencyName: "Tourist Emergency Assistance",
+        foreignEmergencyNumber: "+263-242-70-00-00"
+    }
+};
+
+const emergencyNumbers = Object.values(africanEmergencyNumbers).map(country => ({
+    country:country.countryName,
+    img:country.flag,
+    police: country.police,
+    ambulance: country.ambulance,
+    fire: country.fire,
+    foreignEmergencyName: country.foreignEmergencyName,
+    foreignEmergencyNumber: country.foreignEmergencyNumber
+    
+}));
+function load_emergency( load) {
+emergencyNumbers.map((e)=>{
+    const counties_container = document.querySelector(".Act_Energeency_contacts");
+    const call =`<i class="fa-solid fa-phone"></i>`
+    counties_container.innerHTML += `
+        <div class="country">
+       <div class = "country_name_flag"><p class = "country_name">${e.country}   </ p><figure class = "image_of_Countary"> <img src="${e.img}" alt=""></figure>
+     </div>
+         
+      <div class ="ACT'S_Police flex_contain">
+            <span> Police:  ${e.police}</span>
+            <a href="tel:${e.police}">${call}</a>
+     </div>
+             <div class ="ACT'S_Ambulance  flex_contain">
+            <span>Ambulance: ${e.ambulance}</span>
+            <a href="tel:${e.ambulance}">${call}</a>
+             </div>
+             <div class ="ACT'S_Fire  flex_contain">
+            <span>Fire: ${e.fire}</span>
+            <a href="tel:${e.fire}">${call}</a>
+             </div>
+                <div class ="ACT'S_Foreign  flex_contain">
+            <span>Foreign Affairs Emergency: ${e.foreignEmergencyName}</span>
+            <a href="tel:${e.foreignEmergencyNumber}">${call}</a>
+             </div>
+        </div>
+    `;
+})
+}
+load_emergency()
+const button = document.querySelector("button");
+const input = document.querySelector("#searchInput");
+const countryNames = document.querySelectorAll(".country_name");
+const countryDivs = document.querySelectorAll(".country");
+const countryNameArray = Array.from(countryNames).map(country => country.innerText.toLowerCase());
+const countryDivArray = Array.from(countryDivs);
+const search = (e) => {
+    const searchValue = input.value.toLowerCase();
+    countryDivArray.forEach((countryDiv, index) => {
+        if (countryNameArray[index].includes(searchValue)) {
+            countryDiv.style.display = "block";
+        } else {
+            countryDiv.style.display = "none";
+        }
+    });
+    if (searchValue === "") {
+       alert("Please enter a country name");
+    
+    }
+    if (countryDivArray.every(countryDiv => countryDiv.style.display === "none")) {
+        const countryDivs = document.querySelector(".Act_Energeency_contacts");
+        countryDivs.innerHTML= `
+        <div class="country_notification">
+        <i class=" fa-solid fa-x" onclick = x_lick()></i>
+            <p class="country_name">No country found!!</p> 
+            <span class="country_name">Please enter a valid country name</span>
+            <span class="country_name">ACT's offers emergency service just for African country</span>
+        </div>
+        `;
+    }
+};
+button.addEventListener("click", search);
+    x_lick = () => {
+        const countryDivs = document.querySelector(".Act_Energeency_contacts");
+        countryDivs.innerHTML = ""
+        window.location.reload()
+}
+
+/* const searchInput = document.querySelector("#searchInput");
+searchInput.addEventListener("input", search); */
